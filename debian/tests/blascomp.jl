a0c4374b77f31e410cde33e6fb26c23a2fc33e2a
Mo Zhou <cdluminate@gmail.com>
2018-09-23 06:11:19 +0000

    blascomp.jl: Upgrade to julia 1.0 and add BLIS

--- debian/tests/blascomp.jl
+++ debian/tests/blascomp.jl
@@ -1,17 +1,22 @@
-#!/usr/bin/julia-0.6.2
+#!/usr/bin/julia-1.0
 # Compare performance among different BLAS implementations
 # Reference: Julia/stdlib/LinearAlgebra/src/blas.jl
+using LinearAlgebra
+using Logging
 
 const N1 = 65536  # N for level1 calls
 const N3 = 4096   # N for level3 calls
 const netlibblas = "/usr/lib/x86_64-linux-gnu/blas/libblas.so.3.8.0"
 const atlas      = "/usr/lib/x86_64-linux-gnu/atlas/libblas.so.3.10.3"
-const openblas   = "/usr/lib/x86_64-linux-gnu/libopenblasp-r0.2.20.so"
+const openblas   = "/usr/lib/x86_64-linux-gnu/openblas/libblas.so.3"
 const mkl        = "/usr/lib/x86_64-linux-gnu/libmkl_rt.so"
+const blis       = "/home/lumin/git/blis/lib/haswell/libblis.so"
 
+BLASES = [blis, openblas, mkl]
 
 julia_nrm2 = false
-for libblas in (netlibblas, atlas, openblas, mkl)
+for libblas in BLASES
+	global julia_nrm2
 	@eval begin
 		function ffi_nrm2(n::Integer, X::Union{Ptr{Float64}, AbstractArray{Float64}}, incx::Integer)
 			ccall((:dnrm2_, $libblas), Float64, (Ref{Int64}, Ptr{Float64}, Ref{Int64}), n, X, incx)
@@ -19,20 +24,24 @@ for libblas in (netlibblas, atlas, openblas, mkl)
 	end
 	x = rand(N1)
 	if !julia_nrm2
-		println("\x1b[1;31mdnrm2 Julia\x1b[m")
+		@info("dnrm2 Julia")
 		norm(x)  # JIT
 		@time norm(x)
 		julia_nrm2 = true
 	end
-	println("\x1b[1;31mdnrm2 ", libblas, "\x1b[m")
+	@info("dnrm2 $libblas")
 	ffi_nrm2(N1, x, 1)  # JIT
 	@time ffi_nrm2(N1, x, 1)
 
-	println("  dnrm2 Error :", abs(norm(x) - ffi_nrm2(N1, x, 1)))  # correctness
+	error = abs(norm(x) - ffi_nrm2(N1, x, 1))
+	if error > 1e-7
+		@warn("dnrm2 Error : $error") # correctness
+	end
 end
 
 julia_dgemm = false
-for libblas in (netlibblas, atlas, openblas, mkl)
+for libblas in BLASES
+	global julia_dgemm
 	@eval begin
 		function ffi_gemm!(transA::Char, transB::Char,
 						   alpha::Float64, A::AbstractVecOrMat{Float64},
@@ -42,7 +51,7 @@ for libblas in (netlibblas, atlas, openblas, mkl)
             ka = size(A, transA == 'N' ? 2 : 1)
             kb = size(B, transB == 'N' ? 1 : 2)
             n = size(B, transB == 'N' ? 2 : 1) 
-            ccall((:dgemm_, $libblas), Void,
+            ccall((:dgemm_, $libblas), Cvoid,
                 (Ref{UInt8}, Ref{UInt8}, Ref{Int64}, Ref{Int64},
                  Ref{Int64}, Ref{Float64}, Ptr{Float64}, Ref{Int64},
                  Ptr{Float64}, Ref{Int64}, Ref{Float64}, Ptr{Float64},
@@ -55,16 +64,19 @@ for libblas in (netlibblas, atlas, openblas, mkl)
 	end
 	x, y, z = rand(N3, N3), rand(N3, N3), zeros(N3, N3)
 	if !julia_dgemm
-		println("\x1b[1;31mdgemm Julia\x1b[m")
-		Base.BLAS.gemm('N', 'N', x, y)  # JIT
-		@time Base.BLAS.gemm('N', 'N', x, y)
+		@info("dgemm Julia")
+		BLAS.gemm('N', 'N', x, y)  # JIT
+		@time BLAS.gemm('N', 'N', x, y)
 		julia_dgemm = true
 	end
-	println("\x1b[1;31mdgemm ", libblas, "\x1b[m")
+	@info("dgemm $libblas")
 	ffi_gemm!('N', 'N', 1., x, y, 0., z)  # JIT
 	@time ffi_gemm!('N', 'N', 1., x, y, 0., z)
 
-	z2 = Base.BLAS.gemm('N', 'N', x, y)
+	z2 = BLAS.gemm('N', 'N', x, y)
 	ffi_gemm!('N', 'N', 1., x, y, 0., z)
-	println("  dgemm Error :", norm(z2 - z))  # correctness
+	error = norm(z2 - z)
+	if error > 1e-7
+		@warn("dgemm Error : $error")  # correctness
+	end
 end
