7b9d3306f562482b70abe84d94f88c5afa418f16
Mo Zhou <cdluminate@gmail.com>
2018-09-25 05:58:58 +0000

    fix regex in control.bpo.py

--- debian/control.bpo.py
+++ debian/control.bpo.py
@@ -8,9 +8,7 @@ for (n, line) in enumerate(lines, 1):
     m = re.match('(.*f[\'"]{1}.*[\'"]{1}.*)', line)
     if m:
         print(n, line)
-        x = re.sub('(f)([\'"]{1})(.*)([\'"]{1})', '\\2\\3\\4.format(**locals())', line)
-        x = re.sub('(f)([\'"]{1})(.*)([\'"]{1})', '\\2\\3\\4.format(**locals())', x)
-        x = re.sub('(f)([\'"]{1})(.*)([\'"]{1})', '\\2\\3\\4.format(**locals())', x)
+        x = re.sub('(f)([\'"]{1})(.*?)([\'"]{1})', '\\2\\3\\4.format(**locals())', line)
         print('->', x)
         nlines.append(x)
     else:
