bd04ed6de48864771c2551763133ef151b4d8ab5
Mo Zhou <cdluminate@gmail.com>
2018-12-10 02:48:04 +0000

    test: update abi-blas.c

--- debian/tests/abi-blas.c
+++ debian/tests/abi-blas.c
@@ -25,6 +25,9 @@ main(int argc, char** argv, char** envp)
 		fprintf(stderr, "%s\n", dlerror());
 		exit(EXIT_FAILURE);
 	}
+	int RowMajor = 101;
+	int NoTrans = 111;
+	char NoTrans_ = 'N';
 
 	{ /* F77BLAS and CBLAS :: sasum */
 		float (*sasum_)(BlasInt*, float*, BlasInt*) = NULL;
@@ -60,21 +63,35 @@ main(int argc, char** argv, char** envp)
 	}
 
 	{ /* F77BLAS and CBLAS :: sgemv */
-		void (*cblas_sgemv)(BlasInt, BlasInt, BlasInt, BlasInt, float, float*, BlasInt, float*, BlasInt, float, float*, BlasInt);
+		void (*sgemv_)(const char*, BlasInt*, BlasInt*, float*, float*, BlasInt*, float*, BlasInt*, float*, float*, BlasInt*);
+		void (*cblas_sgemv)(int, int, BlasInt, BlasInt, float, float*, BlasInt, float*, BlasInt, float, float*, BlasInt);
 
 		float A[] = {1., 2., 3., 4.};
 		BlasInt m = 2;
 		BlasInt lda = 2, incx = 1, incy = 1;
+		float alpha = 1.0, beta = 0.0;
 		float X[] = {1., 1.};
 		float Y[] = {0., 0.};
 
-		fprintf(stdout, "%s::cblas_sgemv .. ", LIBBLAS_SO);
-		cblas_sgemv = (void (*)(BlasInt, BlasInt, BlasInt, BlasInt, float, float*, BlasInt, float*, BlasInt, float, float*, BlasInt)) dlsym(handle, "cblas_sgemv");
+		fprintf(stdout, "%s::sgemv_ .. ", LIBBLAS_SO);
+		sgemv_ = (void (*)(const char*, BlasInt*, BlasInt*, float*, float*, BlasInt*, float*, BlasInt*, float*, float*, BlasInt*)) dlsym(handle, "sgemv_");
 		error = dlerror();
 		if (error != NULL) {
 			fprintf(stdout, "NotFound\n");
 		} else {
-			cblas_sgemv(101, 111, m, m, 1., A, lda, X, incx, 0., Y, incy);
+			sgemv_(&NoTrans_, &m, &m, &alpha, A, &lda, X, &incx, &beta, Y, &incy);
+			assert(4. == Y[0]);
+			assert(6. == Y[1]);
+			fprintf(stdout, "OK\n");
+		}
+
+		fprintf(stdout, "%s::cblas_sgemv .. ", LIBBLAS_SO);
+		cblas_sgemv = (void (*)(int, int, BlasInt, BlasInt, float, float*, BlasInt, float*, BlasInt, float, float*, BlasInt)) dlsym(handle, "cblas_sgemv");
+		error = dlerror();
+		if (error != NULL) {
+			fprintf(stdout, "NotFound\n");
+		} else {
+			cblas_sgemv(RowMajor, NoTrans, m, m, alpha, A, lda, X, incx, beta, Y, incy);
 			assert(3. == Y[0]);
 			assert(7. == Y[1]);
 			fprintf(stdout, "OK\n");
@@ -82,21 +99,35 @@ main(int argc, char** argv, char** envp)
 	}
 
 	{ /* F77BLAS and CBLAS :: sgemm */
-		void (*cblas_sgemm)(BlasInt, BlasInt, BlasInt, BlasInt, BlasInt, BlasInt, float, float*, BlasInt, float*, BlasInt, float, float*, BlasInt);
+		void (*sgemm_)(const char*, const char*, BlasInt*, BlasInt*, BlasInt*, float*, float*, BlasInt*, float*, BlasInt*, float*, float*, BlasInt*);
+		void (*cblas_sgemm)(int, int, int, BlasInt, BlasInt, BlasInt, float, float*, BlasInt, float*, BlasInt, float, float*, BlasInt);
 
 		float A[] = {1., 2., 3., 4.};
 		BlasInt m = 2;
 		BlasInt lda = 2, ldb = 2, ldc = 2;
+		float alpha = 1.0, beta = 0.0;
 		float B[] = {1., 1., 1., 1.};
 		float C[] = {0., 0., 0., 0.};
 
-		fprintf(stdout, "%s::cblas_sgemm .. ", LIBBLAS_SO);
-		cblas_sgemm = (void (*)(BlasInt, BlasInt, BlasInt, BlasInt, BlasInt, BlasInt, float, float*, BlasInt, float*, BlasInt, float, float*, BlasInt)) dlsym(handle, "cblas_sgemm");
+		fprintf(stdout, "%s::sgemm_ .. ", LIBBLAS_SO);
+		sgemm_ = (void (*)(const char*, const char*, BlasInt*, BlasInt*, BlasInt*, float*, float*, BlasInt*, float*, BlasInt*, float*, float*, BlasInt*)) dlsym(handle, "sgemm_");
 		error = dlerror();
 		if (error != NULL) {
 			fprintf(stdout, "NotFound\n");
 		} else {
-			cblas_sgemm(101, 111, 111, m, m, m, 1., A, lda, B, ldb, 0., C, ldc);
+			sgemm_(&NoTrans_, &NoTrans_, &m, &m, &m, &alpha, A, &lda, B, &ldb, &beta, C, &ldc);
+			assert(4. == C[0]); assert(6. == C[1]);
+			assert(4. == C[2]); assert(6. == C[3]);
+			fprintf(stdout, "OK\n");
+		}
+
+		fprintf(stdout, "%s::cblas_sgemm .. ", LIBBLAS_SO);
+		cblas_sgemm = (void (*)(int, int, int, BlasInt, BlasInt, BlasInt, float, float*, BlasInt, float*, BlasInt, float, float*, BlasInt)) dlsym(handle, "cblas_sgemm");
+		error = dlerror();
+		if (error != NULL) {
+			fprintf(stdout, "NotFound\n");
+		} else {
+			cblas_sgemm(RowMajor, NoTrans, NoTrans, m, m, m, alpha, A, lda, B, ldb, beta, C, ldc);
 			assert(3. == C[0]); assert(3. == C[1]);
 			assert(7. == C[2]); assert(7. == C[3]);
 			fprintf(stdout, "OK\n");
